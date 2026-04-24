af3026a2c812a73cbbf2666fb45306e9406d7077
jamaludeen750@gmail.com <jamal@MyPC.>
2026-04-24 05:53:24 +0000

    Added : PHP insert into MySQL database

--- database/index.php
+++ database/index.php
@@ -1,5 +1,24 @@
 <?php
 include("database.php");
+
+$username = "John";
+$password = "Football";
+$hash = password_hash($password, PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO users (user, password) 
+        VALUES ('$username','$hash')";
+
+
+try {
+    mysqli_query($conn, $sql);
+    echo "User is registered";
+} catch (mysqli_sql_exception) {
+    echo "Could not register user";
+}
+
+
+
+mysqli_close($conn);
 ?>
 
 <!DOCTYPE html>
